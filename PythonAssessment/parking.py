32e92d319e5f7b5ee4366766dc62430336b1fae6
Felipe Uribe <felipenorg@hotmail.es>
2024-08-22 02:53:30 -0500

    Parking class was created to separate responsibilities and to clean the code.

---
+++ PythonAssessment/parking.py
@@ -0,0 +1,18 @@
+FREE_SLOT = "_"
+BUSSY_SLOT = "x"
+
+class Parking:
+
+    def __init__(self, size: int):
+        self.parking_grid = []
+        self.free_slot = FREE_SLOT
+        self.bussy_slot = BUSSY_SLOT
+        self.fill_parking(size)
+        self.size = size
+
+    def fill_parking(self, size):
+        self.parking_grid = [[FREE_SLOT for _ in range(size)] for _ in range(size)]
+
+    def print_parking(self):
+        for parking_row in self.parking_grid:
+            print(parking_row)
